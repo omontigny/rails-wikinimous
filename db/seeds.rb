36bbc4d6ef2d5ed5cc97372e495d6948c5e98e12
Olivier Montigny <olivier.wagon498@free.fr>
2020-11-18 00:22:43 +0100

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning Database..."
+Article.destroy_all
 
 10.times{
   Article.create(
